77b26481877e4afa78ffa6e13eacc9b5e3c03613
Eliza-Poghosyan <elizapoghosyan22@gmail.com>
2025-09-29 19:06:00 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// // 4)
+// const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"].filter((word) => word.length >6);
+// console.log(words);
+
+// // 6)
+// const people = [
+// { name: "Anna",  age: 17 },
+// { name: "Karen", age: 22 },
+// { name: "Aram",  age: 15 },
+// { name: "Mery",  age: 19 } ];
+// console.log(people.find(person => person.age>18));
+// console.log(people.filter(persons => persons.age>18));
+// const people2 = ['Anna','Karen','Aram','Mery'];
+// console.log(people2);
+
+
+
+
+
